b9fa008b4911a534ff49d191539b57d60cc04663
Luke Shumaker <lukeshu@lukeshu.com>
2022-06-01 09:31:35 -0600

    Add an Item interface

--- pkg/btrfs/btrfsitem/items.go
+++ pkg/btrfs/btrfsitem/items.go
@@ -1,20 +1,11 @@
 package btrfsitem
 
-import "lukeshu.com/btrfs-tools/pkg/btrfs/internal"
+import (
+	"lukeshu.com/btrfs-tools/pkg/btrfs/internal"
+)
 
 type Type = internal.ItemType
 
-const (
-	CHUNK_ITEM_KEY           = internal.CHUNK_ITEM_KEY
-	DEV_ITEM_KEY             = internal.DEV_ITEM_KEY
-	DEV_EXTENT_KEY           = internal.DEV_EXTENT_KEY
-	UNTYPED_KEY              = internal.UNTYPED_KEY
-	QGROUP_RELATION_KEY      = internal.QGROUP_RELATION_KEY
-	INODE_ITEM_KEY           = internal.INODE_ITEM_KEY
-	INODE_REF_KEY            = internal.INODE_REF_KEY
-	ORPHAN_ITEM_KEY          = internal.ORPHAN_ITEM_KEY
-	PERSISTENT_ITEM_KEY      = internal.PERSISTENT_ITEM_KEY
-	ROOT_ITEM_KEY            = internal.ROOT_ITEM_KEY
-	UUID_SUBVOL_KEY          = internal.UUID_SUBVOL_KEY
-	UUID_RECEIVED_SUBVOL_KEY = internal.UUID_RECEIVED_SUBVOL_KEY
-)
+type Item interface {
+	isItem()
+}
---
+++ pkg/btrfs/btrfsitem/items_gen.go
@@ -0,0 +1,29 @@
+package btrfsitem
+
+import "lukeshu.com/btrfs-tools/pkg/btrfs/internal"
+
+const (
+	CHUNK_ITEM_KEY           = internal.CHUNK_ITEM_KEY
+	DEV_ITEM_KEY             = internal.DEV_ITEM_KEY
+	DEV_EXTENT_KEY           = internal.DEV_EXTENT_KEY
+	UNTYPED_KEY              = internal.UNTYPED_KEY
+	QGROUP_RELATION_KEY      = internal.QGROUP_RELATION_KEY
+	INODE_ITEM_KEY           = internal.INODE_ITEM_KEY
+	INODE_REF_KEY            = internal.INODE_REF_KEY
+	ORPHAN_ITEM_KEY          = internal.ORPHAN_ITEM_KEY
+	PERSISTENT_ITEM_KEY      = internal.PERSISTENT_ITEM_KEY
+	ROOT_ITEM_KEY            = internal.ROOT_ITEM_KEY
+	UUID_SUBVOL_KEY          = internal.UUID_SUBVOL_KEY
+	UUID_RECEIVED_SUBVOL_KEY = internal.UUID_RECEIVED_SUBVOL_KEY
+)
+
+func (Chunk) isItem()     {}
+func (Dev) isItem()       {}
+func (DevExtent) isItem() {}
+func (DevStats) isItem()  {}
+func (Empty) isItem()     {}
+func (Inode) isItem()     {}
+func (InodeRef) isItem()  {}
+func (Orphan) isItem()    {}
+func (Root) isItem()      {}
+func (UUIDMap) isItem()   {}
