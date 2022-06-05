65fe72e1daf67d0c9ca3e460e6ec72e0e4a948fe
Luke Shumaker <lukeshu@lukeshu.com>
2022-06-05 11:15:18 -0600

    re-order items.txt

--- pkg/btrfs/btrfsitem/items_gen.go
+++ pkg/btrfs/btrfsitem/items_gen.go
@@ -11,51 +11,51 @@ import (
 const (
 	BLOCK_GROUP_ITEM_KEY     = internal.BLOCK_GROUP_ITEM_KEY
 	CHUNK_ITEM_KEY           = internal.CHUNK_ITEM_KEY
-	DEV_ITEM_KEY             = internal.DEV_ITEM_KEY
 	DEV_EXTENT_KEY           = internal.DEV_EXTENT_KEY
-	DIR_ITEM_KEY             = internal.DIR_ITEM_KEY
+	DEV_ITEM_KEY             = internal.DEV_ITEM_KEY
 	DIR_INDEX_KEY            = internal.DIR_INDEX_KEY
-	XATTR_ITEM_KEY           = internal.XATTR_ITEM_KEY
-	UNTYPED_KEY              = internal.UNTYPED_KEY
-	TREE_BLOCK_REF_KEY       = internal.TREE_BLOCK_REF_KEY
-	SHARED_BLOCK_REF_KEY     = internal.SHARED_BLOCK_REF_KEY
-	QGROUP_RELATION_KEY      = internal.QGROUP_RELATION_KEY
-	EXTENT_ITEM_KEY          = internal.EXTENT_ITEM_KEY
+	DIR_ITEM_KEY             = internal.DIR_ITEM_KEY
 	EXTENT_DATA_REF_KEY      = internal.EXTENT_DATA_REF_KEY
+	EXTENT_ITEM_KEY          = internal.EXTENT_ITEM_KEY
 	INODE_ITEM_KEY           = internal.INODE_ITEM_KEY
 	INODE_REF_KEY            = internal.INODE_REF_KEY
 	METADATA_ITEM_KEY        = internal.METADATA_ITEM_KEY
 	ORPHAN_ITEM_KEY          = internal.ORPHAN_ITEM_KEY
 	PERSISTENT_ITEM_KEY      = internal.PERSISTENT_ITEM_KEY
+	QGROUP_RELATION_KEY      = internal.QGROUP_RELATION_KEY
 	ROOT_ITEM_KEY            = internal.ROOT_ITEM_KEY
+	SHARED_BLOCK_REF_KEY     = internal.SHARED_BLOCK_REF_KEY
 	SHARED_DATA_REF_KEY      = internal.SHARED_DATA_REF_KEY
-	UUID_SUBVOL_KEY          = internal.UUID_SUBVOL_KEY
+	TREE_BLOCK_REF_KEY       = internal.TREE_BLOCK_REF_KEY
+	UNTYPED_KEY              = internal.UNTYPED_KEY
 	UUID_RECEIVED_SUBVOL_KEY = internal.UUID_RECEIVED_SUBVOL_KEY
+	UUID_SUBVOL_KEY          = internal.UUID_SUBVOL_KEY
+	XATTR_ITEM_KEY           = internal.XATTR_ITEM_KEY
 )
 
 var keytype2gotype = map[Type]reflect.Type{
 	BLOCK_GROUP_ITEM_KEY:     reflect.TypeOf(BlockGroup{}),
 	CHUNK_ITEM_KEY:           reflect.TypeOf(Chunk{}),
-	DEV_ITEM_KEY:             reflect.TypeOf(Dev{}),
 	DEV_EXTENT_KEY:           reflect.TypeOf(DevExtent{}),
-	DIR_ITEM_KEY:             reflect.TypeOf(DirList{}),
+	DEV_ITEM_KEY:             reflect.TypeOf(Dev{}),
 	DIR_INDEX_KEY:            reflect.TypeOf(DirList{}),
-	XATTR_ITEM_KEY:           reflect.TypeOf(DirList{}),
-	UNTYPED_KEY:              reflect.TypeOf(Empty{}),
-	TREE_BLOCK_REF_KEY:       reflect.TypeOf(Empty{}),
-	SHARED_BLOCK_REF_KEY:     reflect.TypeOf(Empty{}),
-	QGROUP_RELATION_KEY:      reflect.TypeOf(Empty{}),
-	EXTENT_ITEM_KEY:          reflect.TypeOf(Extent{}),
+	DIR_ITEM_KEY:             reflect.TypeOf(DirList{}),
 	EXTENT_DATA_REF_KEY:      reflect.TypeOf(ExtentDataRef{}),
+	EXTENT_ITEM_KEY:          reflect.TypeOf(Extent{}),
 	INODE_ITEM_KEY:           reflect.TypeOf(Inode{}),
 	INODE_REF_KEY:            reflect.TypeOf(InodeRefList{}),
 	METADATA_ITEM_KEY:        reflect.TypeOf(Metadata{}),
 	ORPHAN_ITEM_KEY:          reflect.TypeOf(Orphan{}),
 	PERSISTENT_ITEM_KEY:      reflect.TypeOf(DevStats{}),
+	QGROUP_RELATION_KEY:      reflect.TypeOf(Empty{}),
 	ROOT_ITEM_KEY:            reflect.TypeOf(Root{}),
+	SHARED_BLOCK_REF_KEY:     reflect.TypeOf(Empty{}),
 	SHARED_DATA_REF_KEY:      reflect.TypeOf(SharedDataRef{}),
-	UUID_SUBVOL_KEY:          reflect.TypeOf(UUIDMap{}),
+	TREE_BLOCK_REF_KEY:       reflect.TypeOf(Empty{}),
+	UNTYPED_KEY:              reflect.TypeOf(Empty{}),
 	UUID_RECEIVED_SUBVOL_KEY: reflect.TypeOf(UUIDMap{}),
+	UUID_SUBVOL_KEY:          reflect.TypeOf(UUIDMap{}),
+	XATTR_ITEM_KEY:           reflect.TypeOf(DirList{}),
 }
 
 func (BlockGroup) isItem()    {}
--- pkg/btrfs/internal/itemtype.go
+++ pkg/btrfs/internal/itemtype.go
@@ -9,52 +9,52 @@ type ItemType uint8
 const (
 	BLOCK_GROUP_ITEM_KEY     = ItemType(192)
 	CHUNK_ITEM_KEY           = ItemType(228)
-	DEV_ITEM_KEY             = ItemType(216)
 	DEV_EXTENT_KEY           = ItemType(204)
-	DIR_ITEM_KEY             = ItemType(84)
+	DEV_ITEM_KEY             = ItemType(216)
 	DIR_INDEX_KEY            = ItemType(96)
-	XATTR_ITEM_KEY           = ItemType(24)
-	UNTYPED_KEY              = ItemType(0)
-	TREE_BLOCK_REF_KEY       = ItemType(176)
-	SHARED_BLOCK_REF_KEY     = ItemType(182)
-	QGROUP_RELATION_KEY      = ItemType(246)
-	EXTENT_ITEM_KEY          = ItemType(168)
+	DIR_ITEM_KEY             = ItemType(84)
 	EXTENT_DATA_REF_KEY      = ItemType(178)
+	EXTENT_ITEM_KEY          = ItemType(168)
 	INODE_ITEM_KEY           = ItemType(1)
 	INODE_REF_KEY            = ItemType(12)
 	METADATA_ITEM_KEY        = ItemType(169)
 	ORPHAN_ITEM_KEY          = ItemType(48)
 	PERSISTENT_ITEM_KEY      = ItemType(249)
+	QGROUP_RELATION_KEY      = ItemType(246)
 	ROOT_ITEM_KEY            = ItemType(132)
+	SHARED_BLOCK_REF_KEY     = ItemType(182)
 	SHARED_DATA_REF_KEY      = ItemType(184)
-	UUID_SUBVOL_KEY          = ItemType(251)
+	TREE_BLOCK_REF_KEY       = ItemType(176)
+	UNTYPED_KEY              = ItemType(0)
 	UUID_RECEIVED_SUBVOL_KEY = ItemType(252)
+	UUID_SUBVOL_KEY          = ItemType(251)
+	XATTR_ITEM_KEY           = ItemType(24)
 )
 
 func (t ItemType) String() string {
 	names := map[ItemType]string{
 		BLOCK_GROUP_ITEM_KEY:     "BLOCK_GROUP_ITEM",
 		CHUNK_ITEM_KEY:           "CHUNK_ITEM",
-		DEV_ITEM_KEY:             "DEV_ITEM",
 		DEV_EXTENT_KEY:           "DEV_EXTENT",
-		DIR_ITEM_KEY:             "DIR_ITEM",
+		DEV_ITEM_KEY:             "DEV_ITEM",
 		DIR_INDEX_KEY:            "DIR_INDEX",
-		XATTR_ITEM_KEY:           "XATTR_ITEM",
-		UNTYPED_KEY:              "UNTYPED",
-		TREE_BLOCK_REF_KEY:       "TREE_BLOCK_REF",
-		SHARED_BLOCK_REF_KEY:     "SHARED_BLOCK_REF",
-		QGROUP_RELATION_KEY:      "QGROUP_RELATION",
-		EXTENT_ITEM_KEY:          "EXTENT_ITEM",
+		DIR_ITEM_KEY:             "DIR_ITEM",
 		EXTENT_DATA_REF_KEY:      "EXTENT_DATA_REF",
+		EXTENT_ITEM_KEY:          "EXTENT_ITEM",
 		INODE_ITEM_KEY:           "INODE_ITEM",
 		INODE_REF_KEY:            "INODE_REF",
 		METADATA_ITEM_KEY:        "METADATA_ITEM",
 		ORPHAN_ITEM_KEY:          "ORPHAN_ITEM",
 		PERSISTENT_ITEM_KEY:      "PERSISTENT_ITEM",
+		QGROUP_RELATION_KEY:      "QGROUP_RELATION",
 		ROOT_ITEM_KEY:            "ROOT_ITEM",
+		SHARED_BLOCK_REF_KEY:     "SHARED_BLOCK_REF",
 		SHARED_DATA_REF_KEY:      "SHARED_DATA_REF",
-		UUID_SUBVOL_KEY:          "UUID_KEY_SUBVOL",
+		TREE_BLOCK_REF_KEY:       "TREE_BLOCK_REF",
+		UNTYPED_KEY:              "UNTYPED",
 		UUID_RECEIVED_SUBVOL_KEY: "UUID_KEY_RECEIVED_SUBVOL",
+		UUID_SUBVOL_KEY:          "UUID_KEY_SUBVOL",
+		XATTR_ITEM_KEY:           "XATTR_ITEM",
 	}
 	if name, ok := names[t]; ok {
 		return name
