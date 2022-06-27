09e3317a433add776d6539abcd7cf4f00ca984e5
Luke Shumaker <lukeshu@lukeshu.com>
2022-06-26 19:57:21 -0600

    add scripts

---
+++ scripts/dump-tree-diff.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+make -C ../btrfs-progs
+diff -u \
+     <(../btrfs-progs/btrfs inspect dump-tree --noscan ../scratch/new.img | sed -e 's/ UNKNOWN.0 / UNTYPED /g' -e 's/\b18446744073709551615\b/-1/g')  \
+     <(go run ./cmd/btrfs-dump-tree/ ../scratch/new.img)
---
+++ scripts/run-fsck.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+rm -f ../scratch/dump-scratch.img
+cp --reflink=always ../scratch/dump-{zero,scratch}.img
+chmod 600 ../scratch/dump-scratch.img
+time go run ./cmd/btrfs-fsck ../scratch/dump-scratch.img
+#make -C ../btrfs-progs
+#time ../btrfs-progs/btrfs rescue chunk-recover ../scratch/dump-scratch.img
