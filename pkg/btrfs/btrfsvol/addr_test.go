6012a4e2184d53a8e2c34f487698f9a821d508c0
Luke Shumaker <lukeshu@lukeshu.com>
2022-07-01 00:32:09 -0600

    gofmt -s

--- pkg/btrfs/btrfsvol/addr_test.go
+++ pkg/btrfs/btrfsvol/addr_test.go
@@ -18,12 +18,12 @@ func TestAddrFormat(t *testing.T) {
 	}
 	addr := btrfsvol.LogicalAddr(0x3a41678000)
 	testcases := map[string]TestCase{
-		"v":   TestCase{InputAddr: addr, InputFmt: "%v", Output: "0x0000003a41678000"},
-		"s":   TestCase{InputAddr: addr, InputFmt: "%s", Output: "0x0000003a41678000"},
-		"q":   TestCase{InputAddr: addr, InputFmt: "%q", Output: `"0x0000003a41678000"`},
-		"x":   TestCase{InputAddr: addr, InputFmt: "%x", Output: "3a41678000"},
-		"d":   TestCase{InputAddr: addr, InputFmt: "%d", Output: "250205405184"},
-		"neg": TestCase{InputAddr: -1, InputFmt: "%v", Output: "-0x000000000000001"},
+		"v":   {InputAddr: addr, InputFmt: "%v", Output: "0x0000003a41678000"},
+		"s":   {InputAddr: addr, InputFmt: "%s", Output: "0x0000003a41678000"},
+		"q":   {InputAddr: addr, InputFmt: "%q", Output: `"0x0000003a41678000"`},
+		"x":   {InputAddr: addr, InputFmt: "%x", Output: "3a41678000"},
+		"d":   {InputAddr: addr, InputFmt: "%d", Output: "250205405184"},
+		"neg": {InputAddr: -1, InputFmt: "%v", Output: "-0x000000000000001"},
 	}
 	for tcName, tc := range testcases {
 		tc := tc
--- pkg/util/uuid_test.go
+++ pkg/util/uuid_test.go
@@ -17,9 +17,9 @@ func TestParseUUID(t *testing.T) {
 		OutputErr string
 	}
 	testcases := map[string]TestCase{
-		"basic":    TestCase{Input: "a0dd94ed-e60c-42e8-8632-64e8d4765a43", OutputVal: util.UUID{0xa0, 0xdd, 0x94, 0xed, 0xe6, 0x0c, 0x42, 0xe8, 0x86, 0x32, 0x64, 0xe8, 0xd4, 0x76, 0x5a, 0x43}},
-		"too-long": TestCase{Input: "a0dd94ed-e60c-42e8-8632-64e8d4765a43a", OutputErr: `too long to be a UUID: "a0dd94ed-e60c-42e8-8632-64e8d4765a43"|"a"`},
-		"bad char": TestCase{Input: "a0dd94ej-e60c-42e8-8632-64e8d4765a43a", OutputErr: `illegal byte in UUID: "a0dd94e"|"j"|"-e60c-42e8-8632-64e8d4765a43a"`},
+		"basic":    {Input: "a0dd94ed-e60c-42e8-8632-64e8d4765a43", OutputVal: util.UUID{0xa0, 0xdd, 0x94, 0xed, 0xe6, 0x0c, 0x42, 0xe8, 0x86, 0x32, 0x64, 0xe8, 0xd4, 0x76, 0x5a, 0x43}},
+		"too-long": {Input: "a0dd94ed-e60c-42e8-8632-64e8d4765a43a", OutputErr: `too long to be a UUID: "a0dd94ed-e60c-42e8-8632-64e8d4765a43"|"a"`},
+		"bad char": {Input: "a0dd94ej-e60c-42e8-8632-64e8d4765a43a", OutputErr: `illegal byte in UUID: "a0dd94e"|"j"|"-e60c-42e8-8632-64e8d4765a43a"`},
 	}
 	for tcName, tc := range testcases {
 		tc := tc
@@ -45,12 +45,12 @@ func TestUUIDFormat(t *testing.T) {
 	}
 	uuid := util.MustParseUUID("a0dd94ed-e60c-42e8-8632-64e8d4765a43")
 	testcases := map[string]TestCase{
-		"s":     TestCase{InputUUID: uuid, InputFmt: "%s", Output: "a0dd94ed-e60c-42e8-8632-64e8d4765a43"},
-		"x":     TestCase{InputUUID: uuid, InputFmt: "%x", Output: "a0dd94ede60c42e8863264e8d4765a43"},
-		"X":     TestCase{InputUUID: uuid, InputFmt: "%X", Output: "A0DD94EDE60C42E8863264E8D4765A43"},
-		"v":     TestCase{InputUUID: uuid, InputFmt: "%v", Output: "a0dd94ed-e60c-42e8-8632-64e8d4765a43"},
-		"40s":   TestCase{InputUUID: uuid, InputFmt: "|% 40s", Output: "|    a0dd94ed-e60c-42e8-8632-64e8d4765a43"},
-		"#115v": TestCase{InputUUID: uuid, InputFmt: "|%#115v", Output: "|           util.UUID{0xa0, 0xdd, 0x94, 0xed, 0xe6, 0xc, 0x42, 0xe8, 0x86, 0x32, 0x64, 0xe8, 0xd4, 0x76, 0x5a, 0x43}"},
+		"s":     {InputUUID: uuid, InputFmt: "%s", Output: "a0dd94ed-e60c-42e8-8632-64e8d4765a43"},
+		"x":     {InputUUID: uuid, InputFmt: "%x", Output: "a0dd94ede60c42e8863264e8d4765a43"},
+		"X":     {InputUUID: uuid, InputFmt: "%X", Output: "A0DD94EDE60C42E8863264E8D4765A43"},
+		"v":     {InputUUID: uuid, InputFmt: "%v", Output: "a0dd94ed-e60c-42e8-8632-64e8d4765a43"},
+		"40s":   {InputUUID: uuid, InputFmt: "|% 40s", Output: "|    a0dd94ed-e60c-42e8-8632-64e8d4765a43"},
+		"#115v": {InputUUID: uuid, InputFmt: "|%#115v", Output: "|           util.UUID{0xa0, 0xdd, 0x94, 0xed, 0xe6, 0xc, 0x42, 0xe8, 0x86, 0x32, 0x64, 0xe8, 0xd4, 0x76, 0x5a, 0x43}"},
 	}
 	for tcName, tc := range testcases {
 		tc := tc
