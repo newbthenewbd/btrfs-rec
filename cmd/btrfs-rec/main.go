0f853e1ead10347be8c5715d6bb797dd5e643e2f
Luke Shumaker <lukeshu@lukeshu.com>
2022-07-11 00:43:59 -0600

    wip btrfs-rec

---
+++ cmd/btrfs-rec/main.go
@@ -0,0 +1,145 @@
+// Copyright (C) 2022  Luke Shumaker <lukeshu@lukeshu.com>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/datawire/dlib/dgroup"
+	"github.com/datawire/dlib/dlog"
+	"github.com/datawire/ocibuild/pkg/cliutil"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+
+	"git.lukeshu.com/btrfs-progs-ng/lib/btrfs"
+	"git.lukeshu.com/btrfs-progs-ng/lib/btrfsprogs/btrfsutil"
+)
+
+type logLevelFlag struct {
+	logrus.Level
+}
+
+func (lvl *logLevelFlag) Type() string { return "loglevel" }
+func (lvl *logLevelFlag) Set(str string) error {
+	var err error
+	lvl.Level, err = logrus.ParseLevel(str)
+	return err
+}
+
+var _ pflag.Value = (*logLevelFlag)(nil)
+
+type subcommand struct {
+	cobra.Command
+	RunE func(*btrfs.FS, *cobra.Command, []string) error
+}
+
+var inspectors, repairers []subcommand
+
+func main() {
+	logLevelFlag := logLevelFlag{
+		Level: logrus.InfoLevel,
+	}
+	var pvsFlag []string
+
+	argparser := &cobra.Command{
+		Use:   "btrfs-rec {[flags]|SUBCOMMAND}",
+		Short: "Recover (data from) a broken btrfs filesystem",
+
+		Args: cliutil.WrapPositionalArgs(cliutil.OnlySubcommands),
+		RunE: cliutil.RunSubcommands,
+
+		SilenceErrors: true, // main() will handle this after .ExecuteContext() returns
+		SilenceUsage:  true, // our FlagErrorFunc will handle it
+
+		CompletionOptions: cobra.CompletionOptions{ //nolint:exhaustivestruct
+			DisableDefaultCmd: true,
+		},
+	}
+	argparser.SetFlagErrorFunc(cliutil.FlagErrorFunc)
+	argparser.SetHelpTemplate(cliutil.HelpTemplate)
+	argparser.PersistentFlags().Var(&logLevelFlag, "verbosity", "set the verbosity")
+	argparser.PersistentFlags().StringArrayVar(&pvsFlag, "pv", nil, "open the file `physical_volume` as part of the filesystem")
+	if err := argparser.MarkPersistentFlagFilename("pv"); err != nil {
+		panic(err)
+	}
+	if err := argparser.MarkPersistentFlagRequired("pv"); err != nil {
+		panic(err)
+	}
+
+	var openFlag int = os.O_RDONLY
+
+	argparserInspect := &cobra.Command{
+		Use:   "inpsect {[flags]|SUBCOMMAND}",
+		Short: "Inspect (but don't modify) a broken btrfs filesystem",
+
+		Args: cliutil.WrapPositionalArgs(cliutil.OnlySubcommands),
+		RunE: cliutil.RunSubcommands,
+	}
+	argparser.AddCommand(argparserInspect)
+
+	argparserRepair := &cobra.Command{
+		Use:   "repair {[flags]|SUBCOMMAND}",
+		Short: "Repair a broken btrfs filesystem",
+
+		Args: cliutil.WrapPositionalArgs(cliutil.OnlySubcommands),
+		RunE: cliutil.RunSubcommands,
+
+		PersistentPreRunE: func(_ *cobra.Command, _ []string) error {
+			openFlag = os.O_RDWR
+			return nil
+		},
+	}
+	argparser.AddCommand(argparserRepair)
+
+	for _, cmdgrp := range []struct {
+		parent   *cobra.Command
+		children []subcommand
+	}{
+		{argparserInspect, inspectors},
+		{argparserRepair, repairers},
+	} {
+		for _, child := range cmdgrp.children {
+			cmd := child.Command
+			runE := child.RunE
+			cmd.RunE = func(cmd *cobra.Command, args []string) error {
+				ctx := cmd.Context()
+				logger := logrus.New()
+				logger.SetLevel(logLevelFlag.Level)
+				ctx = dlog.WithLogger(ctx, dlog.WrapLogrus(logger))
+
+				grp := dgroup.NewGroup(ctx, dgroup.GroupConfig{
+					EnableSignalHandling: true,
+				})
+				grp.Go("main", func(ctx context.Context) (err error) {
+					maybeSetErr := func(_err error) {
+						if _err != nil && err == nil {
+							err = _err
+						}
+					}
+					fs, err := btrfsutil.Open(openFlag, pvsFlag...)
+					if err != nil {
+						return err
+					}
+					defer func() {
+						maybeSetErr(fs.Close())
+					}()
+
+					cmd.SetContext(ctx)
+					return runE(fs, cmd, args)
+				})
+				return grp.Wait()
+			}
+			cmdgrp.parent.AddCommand(&cmd)
+		}
+	}
+
+	if err := argparser.ExecuteContext(context.Background()); err != nil {
+		fmt.Fprintf(os.Stderr, "%v: error: %v\n", argparser.CommandPath(), err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -8,9 +8,12 @@ go 1.18
 
 require (
 	github.com/datawire/dlib v1.3.0
+	github.com/datawire/ocibuild v0.0.3-0.20220423003204-fc6a4e9f90dc
 	github.com/hashicorp/golang-lru v0.5.4
 	github.com/jacobsa/fuse v0.0.0-20220702091825-13117049f383
-	github.com/sirupsen/logrus v1.6.0
+	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/cobra v1.5.0
+	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.7.1
 	golang.org/x/exp v0.0.0-20220518171630-0b5c67f07fdf
 	golang.org/x/text v0.3.7
@@ -18,11 +21,12 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
 
 replace github.com/jacobsa/fuse => github.com/lukeshu/jacobsa-fuse v0.0.0-20220706162300-f42bfdd0fc53
