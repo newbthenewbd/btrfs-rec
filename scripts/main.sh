22ed471931210004caa0c8366c8a92968e602bd9
Luke Shumaker <lukeshu@lukeshu.com>
2022-09-29 11:06:14 -0600

    scripts

--- scripts/main.sh
+++ scripts/main.sh
@@ -26,16 +26,16 @@ gen $b.gen/1.mappings.json \
 gen $b.gen/2.nodes.zip \
     ./btrfs-rec --pv=$b.img --mappings=$b.gen/1.mappings.json \
     inspect visualize-nodes $b.gen/0.scandevices.json
-gen $b.gen/2.loops.txt \
-    ./btrfs-rec --pv=$b.img --mappings=$b.gen/1.mappings.json \
-    inspect show-loops $b.gen/0.scandevices.json
+# gen $b.gen/2.loops.txt \
+#     ./btrfs-rec --pv=$b.img --mappings=$b.gen/1.mappings.json \
+#     inspect show-loops $b.gen/0.scandevices.json
 # gen $b.gen/2.nodes.json \
 #     ./btrfs-rec --pv=$b.img --mappings=$b.gen/1.mappings.json \
 #     inspect rebuild-nodes $b.gen/0.scandevices.json
 
-# gen $b.gen/3.ls-files.txt \
-#     ./btrfs-rec --pv=$b.img --mappings=$b.gen/1.mappings.json \
-#     inspect ls-files
-# gen $b.gen/3.ls-trees.txt \
-#     ./btrfs-rec --pv=$b.img --mappings=$b.gen/1.mappings.json \
-#     inspect ls-trees
+gen $b.gen/3.ls-files.txt \
+    ./btrfs-rec --pv=$b.img --mappings=$b.gen/1.mappings.json \
+    inspect ls-files
+gen $b.gen/3.ls-trees.txt \
+    ./btrfs-rec --pv=$b.img --mappings=$b.gen/1.mappings.json \
+    inspect ls-trees
