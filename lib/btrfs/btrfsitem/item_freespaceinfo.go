53a2ce3b4dcbfd17ac15230da17130a2aeae42dc
Luke Shumaker <lukeshu@lukeshu.com>
2022-10-17 00:15:06 -0600

    Add and improve comments

--- lib/btrfs/btrfsitem/item_freespaceinfo.go
+++ lib/btrfs/btrfsitem/item_freespaceinfo.go
@@ -6,10 +6,28 @@ package btrfsitem
 
 import (
 	"git.lukeshu.com/btrfs-progs-ng/lib/binstruct"
+	"git.lukeshu.com/btrfs-progs-ng/lib/fmtutil"
 )
 
+// key.objectid = object ID of the BlockGroup (logical_addr)
+// key.offset = offset of the BlockGroup (size)
 type FreeSpaceInfo struct { // FREE_SPACE_INFO=198
-	ExtentCount   int32  `bin:"off=0, siz=4"`
-	Flags         uint32 `bin:"off=4, siz=4"`
+	ExtentCount   int32          `bin:"off=0, siz=4"`
+	Flags         FreeSpaceFlags `bin:"off=4, siz=4"`
 	binstruct.End `bin:"off=8"`
+}
+
+type FreeSpaceFlags uint32
+
+const (
+	FREE_SPACE_USING_BITMAPS = FreeSpaceFlags(1 << iota)
+)
+
+var freeSpaceFlagNames = []string{
+	"USING_BITMAPS",
+}
+
+func (f FreeSpaceFlags) Has(req FreeSpaceFlags) bool { return f&req == req }
+func (f FreeSpaceFlags) String() string {
+	return fmtutil.BitfieldString(f, freeSpaceFlagNames, fmtutil.HexNone)
 }
