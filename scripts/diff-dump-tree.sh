2cb3cb14dd85535a0a691d42874d7045d7c81571
Luke Shumaker <lukeshu@lukeshu.com>
2023-02-12 11:28:37 -0700

    scripts/diff-dump-tree.sh: Learn how to run ./autogen.sh and ./configure

--- scripts/diff-dump-tree.sh
+++ scripts/diff-dump-tree.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-make -C ../btrfs-progs
+set -e
+(
+	cd ../btrfs-progs
+	if ! test -f configure; then
+		./autogen.sh
+	fi
+	if ! test -f config.status; then
+		./configure \
+			--disable-documentation \
+			--enable-experimental
+	fi
+	make
+)
 diff -u \
      <(../btrfs-progs/btrfs inspect dump-tree --noscan --csum-items ../scratch/new.img | sed -e 's/ UNKNOWN.0 / UNTYPED /g' -e 's/\b18446744073709551615\b/-1/g')  \
      <(go run ./cmd/btrfs-rec/ inspect dump-trees --pv=../scratch/new.img)
