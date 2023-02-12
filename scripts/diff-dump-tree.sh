37a7aff4df234151f7e68ac4180b5c88a5e02bcf
Luke Shumaker <lukeshu@lukeshu.com>
2023-02-12 03:52:46 -0700

    scripts/diff-dump-tree.sh: Post-process our output to remove thousands-separators

--- scripts/diff-dump-tree.sh
+++ scripts/diff-dump-tree.sh
@@ -13,5 +13,11 @@ set -e
 	make
 )
 diff -u \
-     <(../btrfs-progs/btrfs inspect dump-tree --noscan --csum-items ../scratch/new.img | sed -e 's/ UNKNOWN.0 / UNTYPED /g' -e 's/\b18446744073709551615\b/-1/g')  \
-     <(go run ./cmd/btrfs-rec/ inspect dump-trees --pv=../scratch/new.img)
+     <(../btrfs-progs/btrfs inspect dump-tree --noscan --csum-items ../scratch/new.img |
+	       sed -e 's/ UNKNOWN.0 / UNTYPED /g' \
+		   -e 's/\b18446744073709551615\b/-1/g' \
+		   ) \
+     <(go run ./cmd/btrfs-rec/ inspect dump-trees --pv=../scratch/new.img |
+	       sed -E \
+		   -e 's/([0-9]),/\1/g' \
+		   )
