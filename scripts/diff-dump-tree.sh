ea319c1b817e65ad0808f08343b3203e62ab15a9
Luke Shumaker <lukeshu@lukeshu.com>
2023-02-12 11:48:38 -0700

    scripts: Clean up the scripts to be clearer and more consistent

--- scripts/diff-dump-tree.sh
+++ scripts/diff-dump-tree.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+readonly image=../scratch/new.img
+
+######################################################################
+
 set -e
 (
 	cd ../btrfs-progs
@@ -12,14 +16,19 @@ set -e
 	fi
 	make
 )
+
+make build
+
+######################################################################
+
 diff -u \
-     <(../btrfs-progs/btrfs inspect dump-tree --noscan --csum-items ../scratch/new.img |
+     <(../btrfs-progs/btrfs inspect dump-tree --noscan --csum-items "$image" |
 	       sed -e 's/ UNKNOWN.0 / UNTYPED /g' \
 		   -e 's/\b18446744073709551615\b/-1/g' \
 		   -e 's/INODE_REF 6)/INODE_REF ROOT_TREE_DIR)/g' \
 		   -e 's/ROOT_BACKREF 5)/ROOT_BACKREF FS_TREE)/g' \
 		   ) \
-     <(go run ./cmd/btrfs-rec/ inspect dump-trees --pv=../scratch/new.img |
+     <(./bin/btrfs-rec inspect dump-trees --pv="$image" |
 	       sed -E \
 		   -e 's/([0-9]),/\1/g' \
 		   )
