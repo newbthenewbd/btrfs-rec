c42e8ea5d77a39d096d97600359bd91657da2f05
Luke Shumaker <lukeshu@lukeshu.com>
2023-03-15 21:03:46 -0600

    btrfsprim: Move Key to a separate file

--- lib/btrfs/btrfsprim/misc.go
+++ lib/btrfs/btrfsprim/misc.go
@@ -5,83 +5,12 @@
 package btrfsprim
 
 import (
-	"fmt"
-	"math"
 	"time"
 
 	"git.lukeshu.com/btrfs-progs-ng/lib/binstruct"
-	"git.lukeshu.com/btrfs-progs-ng/lib/containers"
 )
 
 type Generation uint64
-
-type Key struct {
-	ObjectID      ObjID    `bin:"off=0x0, siz=0x8"` // Each tree has its own set of Object IDs.
-	ItemType      ItemType `bin:"off=0x8, siz=0x1"`
-	Offset        uint64   `bin:"off=0x9, siz=0x8"` // The meaning depends on the item type.
-	binstruct.End `bin:"off=0x11"`
-}
-
-// mimics print-tree.c:btrfs_print_key()
-func (key Key) Format(tree ObjID) string {
-	switch tree {
-	case UUID_TREE_OBJECTID:
-		return fmt.Sprintf("(%v %v %#08x)",
-			key.ObjectID.Format(tree),
-			key.ItemType,
-			key.Offset)
-	case ROOT_TREE_OBJECTID, QUOTA_TREE_OBJECTID:
-		return fmt.Sprintf("(%v %v %v)",
-			key.ObjectID.Format(tree),
-			key.ItemType,
-			ObjID(key.Offset).Format(tree))
-	default:
-		if key.Offset == math.MaxUint64 {
-			return fmt.Sprintf("(%v %v -1)",
-				key.ObjectID.Format(tree),
-				key.ItemType)
-		} else {
-			return fmt.Sprintf("(%v %v %v)",
-				key.ObjectID.Format(tree),
-				key.ItemType,
-				key.Offset)
-		}
-	}
-}
-
-func (key Key) String() string {
-	return key.Format(0)
-}
-
-var MaxKey = Key{
-	ObjectID: math.MaxUint64,
-	ItemType: math.MaxUint8,
-	Offset:   math.MaxUint64,
-}
-
-func (key Key) Mm() Key {
-	switch {
-	case key.Offset > 0:
-		key.Offset--
-	case key.ItemType > 0:
-		key.ItemType--
-	case key.ObjectID > 0:
-		key.ObjectID--
-	}
-	return key
-}
-
-func (a Key) Compare(b Key) int {
-	if d := containers.NativeCompare(a.ObjectID, b.ObjectID); d != 0 {
-		return d
-	}
-	if d := containers.NativeCompare(a.ItemType, b.ItemType); d != 0 {
-		return d
-	}
-	return containers.NativeCompare(a.Offset, b.Offset)
-}
-
-var _ containers.Ordered[Key] = Key{}
 
 type Time struct {
 	Sec           int64  `bin:"off=0x0, siz=0x8"` // Number of seconds since 1970-01-01T00:00:00Z.
